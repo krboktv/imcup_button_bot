089d0057378be655a0c2e929c1d9b90c1633df09
Кирилл <krboktv@gmail.com>
2018-03-19 17:38:46 +0300

    fix

--- app.js
+++ app.js
@@ -4694,7 +4694,7 @@ bot.dialog('takePlaceInDisput', [
                 return;
             }
             for (let i in disputsArr) {
-                let card = Cards.disputCard(session, disputsArr[i].num, disputsArr[i].whatType, disputsArr[i].match, disputsArr[i].score, disputsArr[i].currency, disputsArr[i].price)
+                let card = Cards.disputCard(session, disputsArr[i].num, disputsArr[i].whatType, disputsArr[i].match, disputsArr[i].score1, disputsArr[i].currency, disputsArr[i].price)
                 let msg = new builder.Message(session).addAttachment(card);
                 session.send(msg);
 
