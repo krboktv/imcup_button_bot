115ad83520c3d037a726970c1a1839795ba2f803
Кирилл <krboktv@gmail.com>
2018-04-13 10:48:49 +0300

    edit token

--- nt.js
+++ nt.js
@@ -1,9 +1,10 @@
-const rp = require('request-promise');
+const rp = require('request-promise'),
+    safeMath = require('./safeMath.js');
 
 function sendNot(_userid, _text) {
     var options = {
         method: 'POST',
-        uri: 'https://api.telegram.org/bot539909670:AAFk7Lxz73lTbtfjf8xIReCwSoEZZpjAlqI/sendMessage',
+        uri: 'https://api.telegram.org/bot597931763:AAFOGZ7zBhCtse-6FPRyp9-WH5BkusSapzo/sendMessage',
         body: {
             'chat_id': _userid,
             'text': _text,
@@ -19,9 +20,11 @@ function sendNot(_userid, _text) {
 }
 
 function sendNtWhenCreateProposal(approvedUsers, name, sum, address, why) {
+    console.log(safeMath.weiToCurrency(sum))
+    console.log(sum)
 	var arr = approvedUsers.split(',');
 	for (let i in arr) {
-		sendNot(arr[i], "Организация <b>"+name+"</b> хочет вывести "+ sum +" на адрес "+address+".\n<b>Причина:</b> "+why+".\n\nДля одобрения перейдите в личный кабинет.");
+		sendNot(arr[i], "Организация <b>"+name+"</b> хочет вывести "+ safeMath.weiToCurrency(sum) +" ETH на адрес "+address+".\n\n<b>Причина:</b> "+why+".\n\nДля одобрения перейдите в личный кабинет.");
 	}
 }
 
